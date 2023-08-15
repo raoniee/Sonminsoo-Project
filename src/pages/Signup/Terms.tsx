458d7557387937c46e24fddfe0a2c6573f2ec6ef
[SW5] 최재혁 <cslzh4433@gmail.com>
2023-08-15 17:08:20 +0900

    Feat: 이메일 인증 구현1

--- src/pages/Signup/Terms.tsx
+++ src/pages/Signup/Terms.tsx
@@ -1,9 +1,9 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Terms = () => {
-  const [isAllCheck, setIsAllCheck] = useState(false);
-
-  console.log("Check", isAllCheck);
+  const [checkList, setCheckList] = useState([""]);
+  const [infoAgreement, setInfoAgreement] = useState(false);
+  const [pledge, setPledge] = useState(false);
 
   return (
     <div>
@@ -11,13 +11,23 @@ const Terms = () => {
       <h2>정의로운 손민수 이용을 위해 약관에 동의해주세요</h2>
       <input
         type="checkbox"
-        checked={isAllCheck}
-        onClick={() => setIsAllCheck(!isAllCheck)}
+        checked={checkList.length === 2 ? true : false}
+        // onClick={() => setIsAllChecked(!isAllChecked)}
       />
       <span>전체동의</span>
-      <input type="checkbox" />
+      <input
+        type="checkbox"
+        checked={infoAgreement}
+        onClick={() => setInfoAgreement(!infoAgreement)}
+      />
       <span>개인정보 수집 및 이용 동의(필수)</span>&#8250;
-      <input type="checkbox" />
+      <input
+        type="checkbox"
+        checked={pledge}
+        onClick={() => {
+          setPledge(!pledge);
+        }}
+      />
       <span>정의로운 손민수 십계명 서약(필수)</span>&#8250;
     </div>
   );
