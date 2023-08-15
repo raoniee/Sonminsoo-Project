326bebf25dfee39f68e674f86500b23eec6fbb22
[SW5] 최재혁 <cslzh4433@gmail.com>
2023-08-16 01:29:56 +0900

    Feat:라우트 기능 구현, svg파일 타입정의

---
+++ src/Routes/Settings.route.tsx
@@ -0,0 +1,6 @@
+import Signup from "../pages/Signup";
+
+export const SettingsRouter = {
+  path: "settings",
+  element: <Signup />, //설정 컴포넌트
+};
---
+++ src/Routes/index.tsx
@@ -0,0 +1,6 @@
+export * from "./Signup.route";
+export * from "./Chatting.route";
+export * from "./Sonminsoo.route";
+export * from "./Feed.route";
+export * from "./Settings.route";
+export * from "./MyPage.route";
