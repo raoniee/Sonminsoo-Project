0921d2177d92449411ffb5048acd31436e89751d
[SW5] 최재혁 <cslzh4433@gmail.com>
2023-08-16 01:29:56 +0900

    Feat:라우트 기능 구현, svg파일 타입정의

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,16 +1,45 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import Main from "./pages/Main";
 import Missing from "./pages/Missing";
+import Login from "./pages/Login/Login";
+import Signup from "./pages/Signup";
+import Terms from "./pages/Signup/Terms";
+import EmailCertification from "./pages/Signup/EmailCertification";
+import EnterMembersInfo from "./pages/Signup/EnterMembersInfo";
+import SignUpComplete from "./pages/Signup/SignUpComplete";
+import {
+  SignupRouter,
+  ChattingRouter,
+  SonminsooRouter,
+  FeedRouter,
+  MyPageRouter,
+  SettingsRouter,
+} from "./Routes";
 
-const Router = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/*" element={<Missing />} />
-      </Routes>
-    </BrowserRouter>
-  );
-};
+const Router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+  },
+  {
+    path: "home",
+    element: <Login />, //홈 화면 컴포넌트
+  },
+  {
+    path: "hotFandom",
+    element: <Login />, //타오르는 팬덤 컴포넌트
+  },
+  { path: "login", element: <Login /> },
+  {
+    path: "fandom/:fandomId",
+    element: <Signup />, //팬덤 상세보기 컴포넌트
+  },
+  SignupRouter,
+  ChattingRouter,
+  SonminsooRouter,
+  FeedRouter,
+  MyPageRouter,
+  SettingsRouter,
+]);
 
 export default Router;
---
+++ src/Routes/MyPage.route.tsx
@@ -0,0 +1,6 @@
+import Signup from "../pages/Signup";
+
+export const MyPageRouter = {
+  path: "myPage",
+  element: <Signup />, //마이페이지 컴포넌트
+};
