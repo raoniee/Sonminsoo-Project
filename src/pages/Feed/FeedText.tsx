abf21f3ea13f0563b3a91cc19220750e95d4d2c9
[SW5] 이주현 <leek71358@gmail.com>
2023-08-15 23:55:29 +0900

    feat:Feed page skeleton

---
+++ src/pages/Feed/FeedText.tsx
@@ -0,0 +1,38 @@
+import React, { useState} from "react";
+import styled from "styled-components";
+
+const FeedText = styled.p`
+  font-size: 15px;
+  font-weight: 500;
+  line-height: 140%;
+  margin: 30px 16px;
+  width: 239px;
+`;
+const More = styled.span`
+  color: #208df1;
+  font-size: 15px;
+  font-weight: 500;
+  cursor: pointer;
+  display: inline;
+`;
+const Text = () => {
+  const [isShowMore, setIsShowMore] = useState<boolean>(false);
+  const textLimit: number = 50;
+  const [comment] = useState<string>(
+    "럽셒 울정꾸기 리허설 영상 ㅜㅜ 무대 올라왔을 때는 마냥 멋지고 좋았는데 ㅜㅜ 저때 비오고 난리 였대요.. ㅜㅜ애들 고생하는거에 맴찢 ㅜㅜㅜ"
+  );
+  const displayedComment = isShowMore ? comment : comment.slice(0, textLimit);
+
+  return (
+    <FeedText>
+      {displayedComment}
+      {comment.length > textLimit && (
+        <More onClick={() => setIsShowMore(!isShowMore)}>
+          {isShowMore ? "..." : "...더보기"}
+        </More>
+      )}
+    </FeedText>
+  );
+};
+
+export default Text;
---
+++ src/pages/Feed/StatusBar.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import styled from "styled-components";
+import edit from "../../assets/images/svg/ic-edit.svg";
+import search from "../../assets/images/svg/ic-search.svg";
+import setting from "../../assets/images/svg/ic-settings.svg";
+
+const Container = styled.div`
+  width: 100%;
+  height: 50px;
+  display: flex;
+  justify-content: space-between;
+  margin: 0 auto;
+`;
+
+const Logo = styled.div`
+  padding: 15px 16px;
+  font-size: 20px;
+  font-weight: 700;
+`;
+
+const Icons = styled.div`
+  display: flex;
+  padding: 13px 16px;
+  > div:not(:last-child) {
+    margin-right: 12px;
+  }
+`;
+
+const Icon = styled.div<IconProps>`
+  cursor: pointer;
+  width: 24px;
+  height: 24px;
+  background: ${({ background }) => `url(${background})`} no-repeat center
+    center;
+  background-size: contain;
+`;
+type IconProps = {
+  background?: string;
+};
+const StatusBar = () => {
+  return (
+    <Container>
+      <Logo>로고</Logo>
+      <Icons>
+        <Icon background={edit} />
+        <Icon background={search} />
+        <Icon background={setting} />
+      </Icons>
+    </Container>
+  );
+};
+
+export default StatusBar;
