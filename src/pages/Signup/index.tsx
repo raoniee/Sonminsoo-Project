3bc9b54fa631c831e2a63bcfe85cdefbe4ab918f
[SW5] 최재혁 <cslzh4433@gmail.com>
2023-08-15 18:29:59 +0900

    이메일 인증, 페이지별 버튼 추가

--- src/pages/Signup/index.tsx
+++ src/pages/Signup/index.tsx
@@ -5,7 +5,8 @@ import EnterMembersInfo from "./EnterMembersInfo";
 import SignUpComplete from "./SignUpComplete";
 
 const Signup = () => {
-  const [emailAuthValidState, setemailAuthValidState] = useState(false);
+  const [allChecked, setAllChecked] = useState(false);
+  const [emailAuthValidState, setemailAuthValidState] = useState(true);
 
   const signupReducer = (_: string, action: string): string => {
     if (action === "terms") return "authEmail";
@@ -23,10 +24,11 @@ const Signup = () => {
   };
   const [signupStep, dispatch] = useReducer(signupReducer, "terms");
 
-  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+  const handleClick = (e: React.MouseEvent<HTMLInputElement>) => {
     const type = e?.currentTarget.dataset.signupstep
       ? e.currentTarget.dataset.signupstep
       : "";
+
     if (type === "complete") {
       console.log("todo:nav login page");
     }
@@ -35,15 +37,54 @@ const Signup = () => {
   return (
     <div>
       <h1>Step</h1>
-      {signupStep === "terms" && <Terms />}
-      {signupStep === "authEmail" && (
-        <EmailCertification setemailAuthValidState={setemailAuthValidState} />
+      {signupStep === "terms" && (
+        <>
+          <Terms setAllChecked={setAllChecked} allChecked={allChecked} />
+          <input
+            type="button"
+            data-signupstep={signupStep}
+            onClick={handleClick}
+            value="다음"
+            disabled={!allChecked}
+          />
+        </>
       )}
-      {signupStep === "addMemberInfo" && <EnterMembersInfo />}
-      {signupStep === "complete" && <SignUpComplete />}
-      <button data-signupstep={signupStep} onClick={handleClick}>
-        다음
-      </button>
+      {signupStep === "authEmail" && (
+        <>
+          <EmailCertification setemailAuthValidState={setemailAuthValidState} />
+          <input
+            type="button"
+            data-signupstep={signupStep}
+            onClick={handleClick}
+            value="다음"
+            disabled={emailAuthValidState}
+          />
+        </>
+      )}
+      {signupStep === "addMemberInfo" && (
+        <>
+          <EnterMembersInfo />
+          <input
+            type="button"
+            data-signupstep={signupStep}
+            onClick={handleClick}
+            value="회원가입 완료"
+            disabled={emailAuthValidState}
+          />
+        </>
+      )}
+      {signupStep === "complete" && (
+        <>
+          <SignUpComplete />
+          <input
+            type="button"
+            data-signupstep={signupStep}
+            onClick={handleClick}
+            value="로그인 하러가기"
+            disabled={emailAuthValidState}
+          />
+        </>
+      )}
     </div>
   );
 };
