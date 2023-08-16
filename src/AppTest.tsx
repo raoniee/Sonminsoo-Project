c5ddb71badae9349bdf8db20b7be6f4c7e2dc56c
Jieun-Y <106078473+Jieun-Y@users.noreply.github.com>
2023-08-16 11:38:47 +0900

    feat: 임시커밋

---
+++ src/AppTest.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ChatContainer from './pages/ChatList';
+import ChatRoom from './pages/ChatRoom';
+
+const AppTest = () => {
+  return (
+    <div>
+      <ChatContainer />
+      <ChatRoom />
+    </div>
+  );
+};
+
+export default AppTest;
