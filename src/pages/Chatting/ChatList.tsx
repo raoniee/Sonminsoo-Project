506b7df17bf858e0eb5361ee30b2945c5cca0bc0
[SW5]용지은 <wldms0180@gmail.com>
2023-08-19 01:51:37 +0900

    feat: 컴포넌트 분리 및 수정

--- src/pages/Chatting/ChatList.tsx
+++ src/pages/Chatting/ChatList.tsx
@@ -1,24 +1,12 @@
-import React, { Component, ComponentRef, SVGProps } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import { ChatFandom } from './ChatFandom';
-
-// const svgUrl = '../../assets/images/svg/';
+import { Link } from 'react-router-dom';
+import FooterNavBar from '../../components/FooterNavBar';
 
 import { ReactComponent as logo } from '../../assets/images/svg/logo.svg';
-import { ReactComponent as IconHome } from '../../assets/images/svg/ic-home-active.svg';
-import { ReactComponent as IconFeed } from '../../assets/images/svg/ic-feed-inactive.svg';
-import { ReactComponent as IconItem } from '../../assets/images/svg/ic-item-inactive.svg';
-import { ReactComponent as IconChat } from '../../assets/images/svg/ic-chat-inactive.svg';
-import { ReactComponent as IconMypage  } from '../../assets/images/svg/ic-mypage-inactive.svg';
 
-export const ChatWindow = styled.div`
-    // border: 1px solid black;
 
-    width: 390px;
-    height: 844px;
-
-    box-sizing: border-box;
-`;
 
 export const ChatHeader = styled.div`
     // background-color: gray;
@@ -34,6 +22,7 @@ export const ChatHeader = styled.div`
 `;
 
 const Logo = styled(logo)`
+
 `;
 
 const ChatListWindow = styled.div`
@@ -45,95 +34,54 @@ const ChatListWindow = styled.div`
 `;
 
 
-const Footer = styled.div`
-    // background-color: pink;
+const LinkItem = styled(Link)`
+    text-decoration: none;
+    cursor: pointer;
 
-    width: 390px;
-    height: 83px;
+    &:visited {
+        color: black;
+    }
+`
 
-    border-top: 1px solid #A5A8B8;
-    border-top-left-radius: 10px;
-    border-top-right-radius: 10px;
-    box-sizing: border-box;
-`;
 
-const FooterIconWrapper = styled.div`
-    // background: skyblue;
+type fandomProps = {
+    id: number,
+    title: string,
+    memberCount: number,
+    image: string,
+}
 
-    width: 100%;
-
-    margin-top: 7px;
-    padding-left: 15px;
-    padding-right: 15px;
-
-    box-sizing: border-box;
-    display: flex;
-    justify-content: space-between;
-`;
-
-type Props = {
-    text: string,
-    Icon: React.ReactNode,
-    active: boolean,
+const fandomMok: fandomProps = {
+    id: 1234,
+    title: "꾹이의 모든 것",
+    memberCount: 22,
+    image: '',
 }
 
 
-const IconWithText = ({ text, Icon, active } : Props) => {
-    const IconGroup = styled.div`
-        // background-color: green;
 
-        width: 50px;
-        height: 40px;
 
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-
-        cursor: pointer;
-
-        & + & {
-            margin-left: 30.25px;
-        }
-    `;
-
-    const IconText = styled.p`
-        margin: 0;
-        margin-top: 1px;
-
-        font-size: 10px;
-        color: ${active ? '#6138F8' : '#A5A8B8'}
-    `;
+const ChatList = () => {
 
     return (
-        <IconGroup>
-            {Icon}
-            <IconText>{text}</IconText>
-        </IconGroup>
-    )
-}
-
-const ChatContainer = () => {
-    return (
-        <ChatWindow>
+        <>
             <ChatHeader>
                 <Logo />
             </ChatHeader>
             <ChatListWindow>
-                <ChatFandom />
+                <LinkItem to="chatroom">
+                    <ChatFandom />
+                </LinkItem>
+                <LinkItem to="chatroom">
+                    <ChatFandom />
+                </LinkItem>
                 <ChatFandom />
             </ChatListWindow>
-            <Footer>
-                <FooterIconWrapper>
-                    <IconWithText text="홈" Icon={<IconHome />} active={true} />
-                    <IconWithText text="피드" Icon={<IconFeed />} active={false} />
-                    <IconWithText text="손민수템" Icon={<IconItem />} active={false} />
-                    <IconWithText text="채팅방" Icon={<IconChat />} active={false} />
-                    <IconWithText text="마이페이지" Icon={<IconMypage />} active={false} />
-                </FooterIconWrapper>
-            </Footer>
-        </ChatWindow>
+            <FooterNavBar />
+        </>
+      
     )
 }
 
 
-export default ChatContainer;
+export default ChatList;
