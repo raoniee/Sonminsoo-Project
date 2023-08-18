5f7f5db690aa86768912e8c8717121341f6c1c42
[SW5]용지은 <wldms0180@gmail.com>
2023-08-19 01:55:37 +0900

    feat: 채팅방 멤버 관리 화면 추가

--- src/pages/Chatting/ChatMember.tsx
+++ src/pages/Chatting/ChatMember.tsx
@@ -1,42 +1,36 @@
-import React from 'react';
-import BackButton from '../../components/Chatting/BackButton';
-import ChatUserInfo from '../../components/Chatting/ChatUserInfo';
-import iconSearch from '../../assets/images/svg/ic-search.svg';
-import * as S from './styles/ChatMember.style';
-
-
+import React from "react";
+import BackButton from "../../components/Chatting/BackButton";
+import ChatUserInfo from "../../components/Chatting/ChatUserInfo";
+import iconSearch from "../../assets/images/svg/ic-search.svg";
+import * as S from "./styles/ChatMember.style";
 
 type MemberProps = {
-    isBan: boolean,
-}
-
+  isBan: boolean;
+};
 
 type userInfoProps = {
-    id: number,
-    fandomId: number,
-    
-}
+  id: number;
+  fandomId: number;
+};
 
 const ChatMember = () => {
-
-    return (
-        <>
-            <S.ChatHeader>
-                <BackButton />
-                <S.ChatTitle>채팅방 멤버 관리</S.ChatTitle>  
-                <S.IconButton src={iconSearch} />
-            </S.ChatHeader>
-            <S.MemberWindow>
-                <ChatUserInfo $ban={false} />
-                <ChatUserInfo $ban={false} />
-                <ChatUserInfo $ban={false} />
-                <ChatUserInfo $ban={false} />
-                <ChatUserInfo $ban={true} />
-                <ChatUserInfo $ban={true} />
-            </S.MemberWindow>
-        </>
-    )
-}
-
+  return (
+    <>
+      <S.ChatHeader>
+        <BackButton />
+        <S.ChatTitle>채팅방 멤버 관리</S.ChatTitle>
+        <S.IconButton src={iconSearch} />
+      </S.ChatHeader>
+      <S.MemberWindow>
+        <ChatUserInfo $ban={false} />
+        <ChatUserInfo $ban={false} />
+        <ChatUserInfo $ban={false} />
+        <ChatUserInfo $ban={false} />
+        <ChatUserInfo $ban={true} />
+        <ChatUserInfo $ban={true} />
+      </S.MemberWindow>
+    </>
+  );
+};
 
 export default ChatMember;
