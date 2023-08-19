d3bbec179e62ae18f6eb02379ed9f36a6e54a7a1
[SW5]용지은 <wldms0180@gmail.com>
2023-08-20 02:32:14 +0900

    test: test파일 삭제

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import AppTest from './AppTest';
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import { Provider } from "react-redux";
 import store from "./redux/config/store";
