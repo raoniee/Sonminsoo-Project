931a3ee83942749fcb6cbb8a9c6fe680dd43bb7a
Raon <raonharu0822@kakao.com>
2023-08-21 11:42:38 +0900

    Feat: reset.css, font.css 추가

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,8 @@ import App from "./App";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import { Provider } from "react-redux";
 import store from "./redux/config/store";
+import "./styles/reset.css";
+import "./assets/fonts/font.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
