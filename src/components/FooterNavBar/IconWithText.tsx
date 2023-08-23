bb40f482e9489a6658a7e022855af84d4320cc23
[SW5]용지은 <wldms0180@gmail.com>
2023-08-23 11:48:02 +0900

    feat: styled-comonent props type 지정

--- src/components/FooterNavBar/IconWithText.tsx
+++ src/components/FooterNavBar/IconWithText.tsx
@@ -11,98 +11,103 @@ import iconMypageInActive from "../../assets/images/svg/ic-mypage-inactive.svg";
 
 import { styled } from "styled-components";
 
+
+
 type IconWithTextProps = {
   text: string;
   pathname: string;
 };
 
+const IconGroup = styled.div`
+  width: 50px;
+  height: 40px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const TabIcon = styled.div<{$iconUrl: string}>`
+  width: 24px;
+  height: 24px;
+  background-image: url(${props => props.$iconUrl});
+  background-repeat: no-repeat;
+  background-position: center;
+`;
+
+const IconText = styled.p<{$active: boolean}>`
+  margin: 0;
+  margin-top: 1px;
+
+  font-size: 10px;
+
+  color: ${props => props.$active ? "#6138F8" : "#A5A8B8"};
+`;
+
+
 const IconWithText = ({ text, pathname }: IconWithTextProps) => {
-  let active = false;
-  let iconUrl;
+  let $active:boolean = false;
+  let $iconUrl: string = '';
 
   switch (text) {
     case "홈":
       if (pathname === "/") {
-        iconUrl = iconHome;
-        active = true;
+        $iconUrl = iconHome;
+        $active = true;
       } else {
-        iconUrl = iconHomeInActive;
-        active = false;
+        $iconUrl = iconHomeInActive;
+        $active = false;
       }
       break;
 
     case "피드":
       if (pathname === "/feed") {
-        iconUrl = iconFeed;
-        active = true;
+        $iconUrl = iconFeed;
+        $active = true;
       } else {
-        iconUrl = iconFeedInActive;
-        active = false;
+        $iconUrl = iconFeedInActive;
+        $active = false;
       }
       break;
 
     case "손민수템":
       if (pathname === "/minsooItem") {
-        iconUrl = iconItem;
-        active = true;
+        $iconUrl = iconItem;
+        $active = true;
       } else {
-        iconUrl = iconItemInActive;
-        active = false;
+        $iconUrl = iconItemInActive;
+        $active = false;
       }
       break;
 
     case "채팅방":
       if (pathname === "/chatting") {
-        iconUrl = iconChat;
-        active = true;
+        $iconUrl = iconChat;
+        $active = true;
       } else {
-        iconUrl = iconChatInActive;
-        active = false;
+        $iconUrl = iconChatInActive;
+        $active = false;
       }
       break;
 
     case "마이페이지":
       if (pathname === "/myPage") {
-        iconUrl = iconMypage;
-        active = true;
+        $iconUrl = iconMypage;
+        $active = true;
       } else {
-        iconUrl = iconMypageInActive;
-        active = false;
+        $iconUrl = iconMypageInActive;
+        $active = false;
       }
       break;
   }
 
-  const IconGroup = styled.div`
-    width: 50px;
-    height: 40px;
-
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  `;
-
-  const TabIcon = styled.div`
-    width: 24px;
-    height: 24px;
-    background-image: url(${iconUrl});
-    background-repeat: no-repeat;
-    background-position: center;
-  `;
-
-  const IconText = styled.p`
-    margin: 0;
-    margin-top: 1px;
-
-    font-size: 10px;
-
-    color: ${active ? "#6138F8" : "#A5A8B8"};
-  `;
 
   return (
     <IconGroup>
-      <TabIcon />
-      <IconText>{text}</IconText>
+      <TabIcon $iconUrl={$iconUrl} />
+      <IconText $active={$active}>{text}</IconText>
     </IconGroup>
   );
 };
+
 export default IconWithText;
