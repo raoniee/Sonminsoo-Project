2320176341731d0bdb1312d4b69a609268db2bba
[SW5]용지은 <wldms0180@gmail.com>
2023-08-23 12:38:10 +0900

    design: Chatwindow css 수정

--- src/pages/Chatting/index.tsx
+++ src/pages/Chatting/index.tsx
@@ -2,10 +2,8 @@ import styled from 'styled-components';
 import { Outlet } from 'react-router-dom';
 
 export const ChatWindow = styled.div`
-    width: 390px;
-    height: 100%;
-
-    box-sizing: border-box;
+    width: 100%;
+    height: 844px;
 `;
 
 
