7221f623ee23c7378012c28286961ea06ee2ba39
Jieun-Y <106078473+Jieun-Y@users.noreply.github.com>
2023-08-23 22:53:14 +0900

    design: ChatUserInfo css 수정

--- src/components/Chatting/ChatUserInfo.tsx
+++ src/components/Chatting/ChatUserInfo.tsx
@@ -6,10 +6,10 @@ import styled from 'styled-components';
 const UserInfoWrapper = styled.div`
     display: flex;
     align-items: center;
+    justify-content: space-between;
 `;
 
 const UserWrapper = styled.div`
-    width: 100%;
     height: 70px;
     display: flex;
     align-items: center;
