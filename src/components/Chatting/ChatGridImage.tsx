6a38c7e216824c8eddb350ac9b0276ac3711f426
Jieun-Y <106078473+Jieun-Y@users.noreply.github.com>
2023-08-23 20:45:18 +0900

    임시커밋

--- src/components/Chatting/ChatGridImage.tsx
+++ src/components/Chatting/ChatGridImage.tsx
@@ -1,33 +1,30 @@
-import React, { useEffect, useState } from 'react';
-import * as S from './style/ChatGridImage.style';
-import { Chat } from '../../types/chattingType';
-import ChatImageViewer from './ChatImageViewer';
+import React, { useEffect, useState } from "react";
+import * as S from "./style/ChatGridImage.style";
+import { Chat } from "../../types/chattingType";
+import ChatImageViewer from "./ChatImageViewer";
 
+const ChatGridImage = ({ imageList }: Chat) => {
+  const [isViewerOpen, setIsViewerOpen] = useState(false);
+  const $imageCount = imageList.length;
+  const $imgUrl = "";
+  const $isMe: boolean = false;
 
+  const openViewerHandler = () => {
+    setIsViewerOpen(true);
+  };
 
-const ChatGridImage = ({imageList}: Chat) => {
-
-    const [isViewerOpen, setIsViewerOpen] = useState(false);
-    const $imageCount = imageList.length;
-    const $imgUrl = '';
-    const $isMe: boolean = false;
-
-    const openViewerHandler = () => {
-        setIsViewerOpen(true);
-    }
-
-    return (
-        <S.GridImageWrapper $isMe={$isMe}>
-            <S.GridImageContainer $imageCount={$imageCount} >
-                {imageList.map((image, index) => (
-                    <S.GridImage 
-                        key={index}
-                        src={image} 
-                        $imageCount={$imageCount} 
-                        onClick={openViewerHandler} 
-                    />
-                ))}
-                {/* <S.GridImage 
+  return (
+    <S.GridImageWrapper $isMe={$isMe}>
+      <S.GridImageContainer $imageCount={$imageCount}>
+        {imageList.map((image, index) => (
+          <S.GridImage
+            key={index}
+            src={image}
+            $imageCount={$imageCount}
+            onClick={openViewerHandler}
+          />
+        ))}
+        {/* <S.GridImage 
                     src={require('../../assets/images/png/mini.png')}
                     // src={$imgUrl}
                     $imageCount={$imageCount} 
@@ -42,18 +39,20 @@ const ChatGridImage = ({imageList}: Chat) => {
                     // src={$imgUrl}
                     $imageCount={$imageCount} 
                 /> */}
-                {$imageCount > 3 && 
-                    <S.ModalImage>
-                        <p>+{($imageCount > 3 && $imageCount - 3)}개</p>
-                    </S.ModalImage>
-                }
-                {isViewerOpen ?
-                    <ChatImageViewer imageList={imageList} setIsViewerOpen={setIsViewerOpen} />
-                    : null
-                }
-            </S.GridImageContainer>
-        </S.GridImageWrapper>
-    )
+        {$imageCount > 3 && (
+          <S.ModalImage>
+            <p>+{$imageCount > 3 && $imageCount - 3}개</p>
+          </S.ModalImage>
+        )}
+        {isViewerOpen ? (
+          <ChatImageViewer
+            imageList={imageList}
+            setIsViewerOpen={setIsViewerOpen}
+          />
+        ) : null}
+      </S.GridImageContainer>
+    </S.GridImageWrapper>
+  );
 };
 
 export default ChatGridImage;
