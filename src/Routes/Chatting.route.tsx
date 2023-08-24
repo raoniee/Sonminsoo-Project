3a4819e603df448ec9b68f9f9a9dcd1abfc0f07c
[SW5]용지은 <wldms0180@gmail.com>
2023-08-25 02:16:21 +0900

    chore: chatting router path 수정

--- src/Routes/Chatting.route.tsx
+++ src/Routes/Chatting.route.tsx
@@ -8,7 +8,7 @@ export const ChattingRouter = {
   element: <Chatting />, //채팅 컴포넌트
   children: [
     { path: "", element: <ChatList /> }, // 채팅 리스트
-    { path: "chatroom", element: <ChatRoom /> },  // 채팅방
-    { path: "chatmember", element: <ChatMember /> }   // 채팅 멤버
+    { path: "chatroom/:id", element: <ChatRoom /> },  // 채팅방
+    { path: "chatmember/:id", element: <ChatMember /> }   // 채팅 멤버
   ],
 };
