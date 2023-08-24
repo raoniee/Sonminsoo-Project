8819902563d712abf3e3c6738748a31061d10f41
[SW5] 최재혁 <cslzh4433@gmail.com>
2023-08-24 11:57:08 +0900

    Feat: 공용 버튼 추가

---
+++ src/elements/Button.tsx
@@ -0,0 +1,16 @@
+import { styled } from "styled-components";
+
+export const Button = styled.button<{ background: string; border: string }>`
+  width: 358px;
+  height: 60px;
+  border-radius: 8px;
+  text-align: center;
+  font-family: Pretendard;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 100%; /* 16px */
+  ${(props) => (props.color ? `color : ${props.color}` : "")};
+  ${(props) => (props.border ? `border : ${props.border}` : "")};
+  ${(props) => (props.background ? `background : ${props.background}` : "")};
+`;
