b2f08b88bc92e2306f3ab20e146d6e425e328551
[SW5] 최재혁 <cslzh4433@gmail.com>
2023-08-24 20:00:06 +0900

    Feat: 버킷리스트 추가 모달 기능 스타일 추가

--- src/components/BucketListModal/BucketItem.tsx
+++ src/components/BucketListModal/BucketItem.tsx
@@ -1,9 +1,25 @@
 import * as S from "./style";
 import test from "../../assets/images/svg/SonminsooItem/bookmarkIcon.svg";
-const BucketItem = () => {
+import React from "react";
+type props = {
+  id: string;
+  img: string;
+  title: string;
+  onClick: React.MouseEventHandler<HTMLElement>;
+  selected: boolean;
+};
+const BucketItem: React.FC<props> = ({
+  id,
+  img,
+  title,
+  onClick,
+  selected = false,
+}) => {
+  console.log("Bucket", selected);
   return (
-    <S.BucketItemContainer>
-      <S.BucketItemImage $url={test} />
+    <S.BucketItemContainer key={id} onClick={onClick}>
+      <S.BucketItemImage $url={img} selected={selected} />
+      <S.BucketItemTitle>{title}</S.BucketItemTitle>
     </S.BucketItemContainer>
   );
 };
