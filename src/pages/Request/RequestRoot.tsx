2b69017366c9404cd0533cd2da14a557d37cb07a
Raon <raonharu0822@kakao.com>
2023-08-25 00:35:42 +0900

    Feat: Request.tsx 수정

--- src/pages/Request/RequestRoot.tsx
+++ src/pages/Request/RequestRoot.tsx
@@ -1,14 +1,8 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import RequestHeader from "../../components/Request/RequestHeader";
 
 const RequestRoot: React.FC = () => {
-  return (
-    <>
-      <RequestHeader />
-      <Outlet />
-    </>
-  );
+  return <Outlet />;
 };
 
 export default RequestRoot;
--- src/pages/Request/Requests.tsx
+++ src/pages/Request/Requests.tsx
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import * as S from "./style/Request.style";
 import RequestList from "../../components/Request/RequestList";
+import document from "../../assets/images/svg/ic-document.svg";
+import HeaderBar from "../../components/common/HeaderBar/HeaderBar";
+import edit from "../../assets/images/svg/ic-edit.svg";
+import Icon from "../../elements/Icon";
 
 const Requests: React.FC = () => {
+  const [request, setRequest] = useState(true); //나중에 ""로 바꾸기
+
   return (
-    <S.Wrap>
-      {/* <RequestList />
-      <RequestList />
-      <RequestList /> */}
-    </S.Wrap>
+    <>
+      <HeaderBar
+        BackButton={true}
+        title="손민수템 의뢰"
+        items={[<Icon src={edit} />]}
+      />
+      <S.Wrap>
+        {!request && (
+          <S.NoRequest>
+            <S.NoRequestIcon src={document} />
+            <S.NoRequestDesc>새 의뢰를 작성해주세요!</S.NoRequestDesc>
+          </S.NoRequest>
+        )}
+        {request && (
+          <RequestList
+            title="제발 이것좀 찾아 주세요 ㅜㅜ"
+            username="아마추어 손민수"
+            date="10분전"
+          />
+        )}
+      </S.Wrap>
+    </>
   );
 };
 
