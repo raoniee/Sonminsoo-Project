2bd1b672ec2ca84db0a2853da5c4957673e057b6
[SW5] 최재혁 <cslzh4433@gmail.com>
2023-08-25 15:41:19 +0900

    Feat :sonminsooItem page optimization

--- src/pages/SonminsooItem/SonminsooItem.tsx
+++ src/pages/SonminsooItem/SonminsooItem.tsx
@@ -1,8 +1,12 @@
 import { Outlet } from "react-router-dom";
 import { useMemo, useState } from "react";
+import { useMemo, useState } from "react";
 import * as S from "./style/SonminsooItem.style";
 import FooterNavBar from "../../components/common/FooterNavBar/FooterNavBar";
 import BucketListModal from "../../components/common/BucketListModal/BucketListModal";
+import Icon from "../../elements/Icon";
+import search from "../../assets/images/svg/SonminsooItem/ic-search.svg";
+import settings from "../../assets/images/svg/SonminsooItem/ic-settings.svg";
 
 type bucketList = {
   id: string;
@@ -18,17 +22,46 @@ const SonminsooItem = () => {
     return bucketList;
   }, [bucketList]); //bucketList 데이터가 변경 될 때만 BucketListModal 렌더링
 
+  const [modalView, setModalView] = useState(false);
+  const [bucketList, setBucketList] = useState<bucketList>([]);
+
+  let bucketListData = useMemo(() => {
+    return bucketList;
+  }, [bucketList]); //bucketList 데이터가 변경 될 때만 BucketListModal 렌더링
+
   return (
     <>
       <S.SonminsooItemContainer>
+        {useMemo(() => {
+          return (
+            <>
+              <HeaderBar
+                BackButton={false}
+                items={[
+                  <Icon src={search} key="search" />,
+                  <Icon src={settings} key="settings" />,
+                ]}
+              />
+              <S.LinkRequestList to="/requests">
+                <span>손민수템 의뢰 리스트</span>
+                <span>&gt;</span>
+              </S.LinkRequestList>
+            </>
+          );
+        }, [])}
+
         <Outlet context={{ setModalView, setBucketList }} />
         {modalView && (
           <BucketListModal
             setModalOpen={setModalView}
             addClickHandler={() => {}}
             bucketList={bucketListData}
+            bucketList={bucketListData}
           />
         )}
+        {useMemo(() => {
+          return <FooterNavBar />;
+        }, [])}
         {useMemo(() => {
           return <FooterNavBar />;
         }, [])}
