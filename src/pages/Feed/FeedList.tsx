83be39c5abdb8937df9f00962c79d685241bfa29
[SW5] 이주현 <leek71358@gmail.com>
2023-08-26 22:05:38 +0900

    feat:피드작성 공지등록, 대상자 글자제한

--- src/pages/Feed/FeedList.tsx
+++ src/pages/Feed/FeedList.tsx
@@ -57,7 +57,6 @@ const FeedList: React.FC = () => {
   const fetchData = async () => {
     try {
       const response = await axios.get("http://localhost:5000/feed");
-      console.log(response);
       setFeedData(response.data);
     } catch (error) {
       console.error("Error", error);
