7a55876ff6b6cb56068ec844879d0e845b3a72ee
[SW5] 최재혁 <cslzh4433@gmail.com>
2023-08-28 02:39:24 +0900

    Feat: props 수정

--- src/pages/SonminsooItem/SonMinsooItemInfo.tsx
+++ src/pages/SonminsooItem/SonMinsooItemInfo.tsx
@@ -25,10 +25,8 @@ type sonminsooItemInfo = {
 };
 const SonMinsooItemInfo = ({
   artistName,
-  groupName,
   id,
   imgUrl,
-  originUrl,
   price,
   title,
   isInMyBucket,
@@ -50,7 +48,6 @@ const SonMinsooItemInfo = ({
       </Link>
       <S.TitleContainer>
         <Link to={`details/${id}`}>
-          {" "}
           <S.ItemTitle>{title}</S.ItemTitle>
         </Link>
         <S.bookMarkIcon
