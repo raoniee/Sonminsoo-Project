2f0d26098480de7575b8ddfdc1770d76288f0f2a
[SW5]용지은 <wldms0180@gmail.com>
2023-08-28 02:28:47 +0900

    fix: placeholder 코드 수정

--- src/components/Chatting/ChatInputBar.tsx
+++ src/components/Chatting/ChatInputBar.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useRef } from 'react';
 import iconImage from "../../assets/images/svg/ic-image.svg";
 import iconCamera from "../../assets/images/svg/ic-camera.svg";
 import * as S from './style/ChatInputBar.style';
@@ -19,22 +19,12 @@ type UploadImage = {
 const ChatBar = ({ban}:Props) => {
 
     // 채팅 정지 상태에 따른 채팅input placeholder 설정
-    const [placeHolder, SetPlaceHolder] = useState('채팅을 입력해 주세요'); 
     const fileInputRef = useRef<HTMLInputElement>(null);
     const [imageFile, setImageFile] = useState<UploadImage>();
 
     // input 파일형식
     const acceptFormat='image/jpg, image/jpeg, image/png';
 
-
-    useEffect(() => {
-        if(ban) {
-            SetPlaceHolder('채팅정지 상태입니다');
-        } else {
-            SetPlaceHolder('채팅을 입력해 주세요');
-        }
-    }, [ban]);
-    
 
     // 이미지 버튼에 input 연결
     const FileUploadClick = (e: React.MouseEvent<HTMLImageElement>) => {
@@ -71,8 +61,11 @@ const ChatBar = ({ban}:Props) => {
                 <S.ChatIconImage src={iconCamera} onClick={FileUploadClick} />
             </S.IconWrapper>
             <S.ChatInputWrapper>
-                <S.ChatInput placeholder={placeHolder} disabled={ban ? true : false} />
-                <S.ChatInputButton disabled={ban ? true : false} />
+                <S.ChatInput 
+                    placeholder={ban ? '채팅정지 상태입니다.' : '채팅을 입력해주세요'} 
+                    disabled={ban} 
+                />
+                <S.ChatInputButton disabled={ban} />
             </S.ChatInputWrapper>
         </S.ChatBarWrapper>
     )
