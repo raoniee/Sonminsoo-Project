7ab83f47f67b78b99b7f30d85e01a922ff3859fa
Raon <raonharu0822@kakao.com>
2023-08-27 21:00:53 +0900

    Feat: requestform api 작성

--- src/pages/Request/RequestDetailWriter.tsx
+++ src/pages/Request/RequestDetailWriter.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import * as S from "./style/RequestDatailWriter.style";
 import RequestNoWriterResponse from "../../components/Request/nowriter/RequestNoWriterResponse";
 import RequestDetailDesc from "../../components/Request/RequestDetailDesc";
-
 import RequestDetailWriterHeader from "../../components/Request/writer/RequestDetailWriterHeader";
 import RequestWriterResponse from "../../components/Request/writer/RequestWriterResponse";
+import HeaderBar from "../../components/common/HeaderBar/HeaderBar";
 
 const RequestDetailWriter: React.FC = () => {
   return (
     <>
+      <HeaderBar BackButton={true} color="#fff" />
       <RequestDetailWriterHeader />
       <RequestDetailDesc />
       <S.ResponseNumber>답변 0개</S.ResponseNumber>
