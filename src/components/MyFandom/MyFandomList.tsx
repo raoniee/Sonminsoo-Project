8dfd55a3ca02eeea5c24252843d8d5498b1dfb5e
[SW5] 신혜정 <shj3229123@gmail.com>
2023-08-27 23:31:02 +0900

    feat: 통신 에러 해결

--- src/components/MyFandom/MyFandomList.tsx
+++ src/components/MyFandom/MyFandomList.tsx
@@ -19,9 +19,9 @@ const MyFandomList: React.FC<FandomdataProps> = ({ item }) => {
             <S.CircleImg />
             <S.RnakingTextBox>
                 <S.FandomNameMemberBox>
-                    <S.FandomNameText>꾹이의 모든것</S.FandomNameText>
+                    <S.FandomNameText>{item.fandomName}</S.FandomNameText>
                     <S.MamberIcon />
-                    <S.MemberQuantity>22k</S.MemberQuantity>
+                    <S.MemberQuantity>{item.memberLength}</S.MemberQuantity>
                 </S.FandomNameMemberBox>
                 <S.ActivityTime>채팅활동 10분전</S.ActivityTime>
             </S.RnakingTextBox>
--- src/pages/MyFandom/MyFandom.tsx
+++ src/pages/MyFandom/MyFandom.tsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux/es/hooks/useSelector";
 import * as S from "./style/MyFandom.style";
-import axios, { axiosPrivate } from "../../api/axios";
+
 import HomeHeader from "../../components/Home/HomeHeader";
 import FandomMenu from "../../components/MyFandom/FandomMenu";
 import ContentHeader from "../../components/MyFandom/ContentHeader";
 import MyFandomList from "../../components/MyFandom/MyFandomList";
+import useAxiosPrivate from "../../hooks/useAxiosPrivate";
 
 type Fandom = {
     fandomName: string;
@@ -19,13 +20,14 @@ type Fandom = {
 type FandomData = Fandom[];
 
 const MyFandom = () => {
+    const axiosPrivate = useAxiosPrivate();
     const [data, setData] = useState<FandomData>([]);
 
-    // const auth = useSelector((state: any) => {
-    //     return state.auth.accessToken;
-    // });
+    const auth = useSelector((state: any) => {
+        return state.auth.accessToken;
+    });
 
-    // console.log("auth:", auth);
+    console.log("auth:", auth);
 
     useEffect(() => {
         initDataGet();
@@ -34,8 +36,8 @@ const MyFandom = () => {
     const initDataGet = async () => {
         try {
             const res = await axiosPrivate.get("/users/fandoms");
-            //console.log("get:", res);
-            //setData(res.data);
+            console.log("get:", res.data.data);
+            setData(res.data.data);
         } catch (error) {
             console.error("Error", error);
         }
@@ -49,9 +51,9 @@ const MyFandom = () => {
             </S.MyFandomHeaderBox>
             <S.MyFandomListBox>
                 <ContentHeader />
-                {/* {data.map((item) => (
+                {data.map((item) => (
                     <MyFandomList key={item.id} item={item} />
-                ))} */}
+                ))}
             </S.MyFandomListBox>
         </S.Container>
     );
