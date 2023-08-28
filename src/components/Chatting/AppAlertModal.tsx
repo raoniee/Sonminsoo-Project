b4be10ee637d5a721da784562a5f23481c8171a2
[SW5]용지은 <wldms0180@gmail.com>
2023-08-28 15:50:33 +0900

    feat: Chat AlertModal 추가

---
+++ src/components/Chatting/AppAlertModal.tsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useRef } from "react";
+import * as S from "../common/AlertModal/style/AppAlertModal.style";
+
+type ModalProps = {
+  setModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  title: string;
+  content: string;
+  warning?: boolean;
+  yesContent: string;
+  yesClickHandler: React.MouseEventHandler<HTMLDivElement>;
+};
+const AppAlertModal: React.FC<ModalProps> = ({
+  setModalOpen,
+  title,
+  content,
+}) => {
+  const modalRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const clickOutside = (e: any) => {
+      if (!modalRef.current?.contains(e.target)) {
+        setModalOpen(false);
+      }
+    };
+    document.addEventListener("mousedown", clickOutside);
+    return () => {
+      document.removeEventListener("mousedown", clickOutside);
+    };
+  }, []);
+
+  return (
+    <S.Overlay>
+      <S.ModalContainer ref={modalRef}>
+        <S.ModalText>
+          <S.ModalTitle>{title}</S.ModalTitle>
+          <S.ModalContent>{content}</S.ModalContent>
+        </S.ModalText>
+        <S.ModalBtn>
+          <S.ModalNo
+            onClick={() => {
+              setModalOpen(false);
+            }}
+          >
+            확인
+          </S.ModalNo>
+        </S.ModalBtn>
+      </S.ModalContainer>
+    </S.Overlay>
+  );
+};
+
+export default AppAlertModal;
