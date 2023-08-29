c10b5e75b4b3ba3b33d36ae8877a94b16eda72ca
[SW5] 신혜정 <shj3229123@gmail.com>
2023-08-29 13:26:57 +0900

    feat: 팬덤 생성 기능 구현

--- src/pages/MyFandom/CreateNewFandom.tsx
+++ src/pages/MyFandom/CreateNewFandom.tsx
@@ -6,19 +6,20 @@ import useInput from "../../hooks/useInput";
 import { useNavigate } from "react-router-dom";
 
 const CreateNewFandom = () => {
+    const axiosPrivate = useAxiosPrivate();
+
     const navigation = useNavigate();
     const fileInputRef = useRef<HTMLInputElement>(null);
-    // 이미지 스트링이랑 인풋 스트링 같이 담아서 포스트 요청
+
     const [selectedImage, setSelectedImage] = useState<string>("");
     const [inputVal, setInputVal] = useInput("");
+
     const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const file = event.target.files?.[0];
 
-        // Non-null assertion operators
         const imageUrl = URL.createObjectURL(file!);
 
         if (file) {
-            // 파일을 선택했을 때 수행할 작업
             setSelectedImage(imageUrl);
         }
     };
@@ -29,17 +30,14 @@ const CreateNewFandom = () => {
         }
     };
 
-    const axiosPrivate = useAxiosPrivate();
-
     const createNewFandomPost = async () => {
-        console.log("사진정보", selectedImage);
         const formData = new FormData();
         formData.append("fandomName", inputVal);
-        formData.append("file", selectedImage);
-        console.log("폼데이터", formData);
-        if (selectedImage && inputVal) {
-            // 이미지와 인풋 값이 모두 존재하는 경우에만 실행되는 로직
+        if (fileInputRef.current?.files?.[0]) {
+            formData.append("file", fileInputRef.current?.files?.[0]);
+        }
 
+        if (selectedImage && inputVal) {
             try {
                 const res = await axiosPrivate.post(
                     "/users/fandoms",
@@ -54,7 +52,6 @@ const CreateNewFandom = () => {
         }
     };
 
-    // 함수 만든거 프룹으로 함수 넘겨주삼 그리고 거기서 온클릭하면 요청 보내지도록
     return (
         <S.Container>
             <S.FandomContainer>
@@ -65,7 +62,7 @@ const CreateNewFandom = () => {
                             새 팬덤 만들기
                         </S.MyFandomHeaderText>
                     </S.MyFandomHeaderBox>
-                    {/* 이미지와 인풋의 값이 있으면 체크 아이콘이 그레이에서 블루로 바뀌도록  */}
+
                     <S.CheckIconBtn
                         style={{
                             filter:
