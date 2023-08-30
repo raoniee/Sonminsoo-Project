c1a5731b44b4bc3f2d7ab33f7c520052dee78e64
[SW5] 최재혁 <cslzh4433@gmail.com>
2023-08-30 11:41:56 +0900

    Feat: 회원가입 페이지 기능 추가 중

--- src/hooks/useAxiosPrivate.tsx
+++ src/hooks/useAxiosPrivate.tsx
@@ -3,20 +3,20 @@ import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 
 const useAxiosPrivate = () => {
-    const auth = useSelector((state: any) => {
-        return state.auth.accessToken;
+  const auth = useSelector((state: any) => {
+    return state.auth.accessToken;
+  });
+
+  useEffect(() => {
+    axiosPrivate.interceptors.request.use((config) => {
+      config.headers["authorization"] = auth;
+      console.log(config, "config");
+      // console.log(auth, "auth");
+
+      return config;
     });
+  }, [auth]);
 
-    useEffect(() => {
-        axiosPrivate.interceptors.request.use((config) => {
-            config.headers["authorization"] = auth;
-            // console.log(config, "config");
-            // console.log(auth, "auth");
-
-            return config;
-        });
-    }, [auth]);
-
-    return axiosPrivate;
+  return axiosPrivate;
 };
 export default useAxiosPrivate;
