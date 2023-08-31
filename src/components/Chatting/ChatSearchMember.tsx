4e4ebc704d087082dd42bd258dbbf6fe125a1ab0
[SW5]용지은 <wldms0180@gmail.com>
2023-09-01 08:58:59 +0900

    feat: 채팅 socket 연동

--- src/components/Chatting/ChatSearchMember.tsx
+++ src/components/Chatting/ChatSearchMember.tsx
@@ -1,39 +1,17 @@
 import React, { useEffect, useState } from 'react';
-import type {ChatMemberType} from '../../types/chattingType';
+import type { MemberType } from '../../types/chattingType';
 import ChatUserInfo from './ChatUserInfo';
 import iconArrow from '../../assets/images/svg/ic-arrow-left.svg';
 import * as S from './style/ChatSearchMember.style';
 
 
 
-type SearchMember = {
+type SearchMemberType = {
     setOpenSearch: React.Dispatch<React.SetStateAction<boolean>>;
+    members: MemberType[];
 }
 
-const memberMock:ChatMemberType[] = [
-    {
-        $ban: false,
-        userName: "아마추어 손민수1",
-    },
-    {
-        $ban: false,
-        userName: "나는야 손민수",
-    },
-    {
-        $ban: false,
-        userName: "손민수 하러 옴",
-    },
-    {
-        $ban: true,
-        userName: "룰라랄라",
-    },
-    {
-        $ban: true,
-        userName: "호에에엥",
-    },
-];
-
-const ChatSearchMember = ({setOpenSearch}: SearchMember) => {
+const ChatSearchMember = ({setOpenSearch, members}: SearchMemberType) => {
 
     const [userInput, setUserInput] = useState<string>('');
     
@@ -41,7 +19,7 @@ const ChatSearchMember = ({setOpenSearch}: SearchMember) => {
         setUserInput(e.target.value);
     }
     
-    const userFiltered = memberMock.filter((member) => member.userName.toUpperCase().includes(userInput));
+    const userFiltered = members.filter((member) => member.nickName.toUpperCase().includes(userInput));
 
     
     return (
@@ -55,10 +33,17 @@ const ChatSearchMember = ({setOpenSearch}: SearchMember) => {
                 />
             </S.SearchHeader>
             <S.SearchMemberContainer>
-            {userInput && userFiltered
+            {/* {userInput && userFiltered */}
+            { userFiltered
                 .map((member, index) => (
                     <React.Fragment key={index}>
-                        <ChatUserInfo $ban={member.$ban} userName={member.userName} />         
+                        <ChatUserInfo  
+                            id={member.id}
+                            isAdmin={member.isAdmin}
+                            nickName={member.nickName}
+                            image={member.image}
+                            isJail={member.isJail}
+                        />         
                     </React.Fragment>
             ))}
             </S.SearchMemberContainer>
--- src/components/Chatting/ChatUserInfo.tsx
+++ src/components/Chatting/ChatUserInfo.tsx
@@ -1,31 +1,92 @@
-import React, { useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import * as S from './style/ChatUserInfo.style';
+import { MemberType, UserInfoType } from '../../types/chattingType';
+import { useLocation, useNavigate } from 'react-router';
+import { SocketContext } from '../../App';
+import { Socket } from 'socket.io-client';
+import { isBrowser } from 'react-device-detect';
+import AppAlertModal from '../common/AlertModal/AppAlertModal';
+import WebAlertModal from '../common/AlertModal/WebAlertModal';
     
 
 
-type MemberProps = {
-    $ban: boolean;
-    userName: string;
-}
-    
-const ChatUserInfo = ({$ban, userName}:MemberProps) => {
+const ChatUserInfo = ({id, isAdmin, nickName, image, isJail}:MemberType) => {
 
-    const [banToggle, setBanToggle] = useState($ban);
-    const chatBanText: string = banToggle ? '채팅정지 취소' : '채팅정지';
+    const socket = useContext<Socket | undefined>(SocketContext);
+    const admin = "관리자";
+    const navigate = useNavigate();
+    const { pathname } = useLocation();
+    const roomId = Number(pathname.split("/chatting/chatmember/")[1]);
+    const [myInfo, setMyInfo] = useState<UserInfoType | undefined>(undefined);
+    const [onAlert, setOnAlert] = useState<boolean>(false);
 
-    const banHandler = () => {
-        banToggle ? setBanToggle(false) : setBanToggle(true);
+    const userBanHandler = () => {
+        if (socket) {
+            socket.emit("jail", {room: roomId, userId: id});
+            setOnAlert(false);
+        }
     }
+
+    useEffect(() => {
+        if(socket) {
+            const getMyInfo = (v: React.SetStateAction<UserInfoType | undefined>) => setMyInfo(v);
+
+            socket.emit("myInfo", roomId);
+            socket.on("myInfo", getMyInfo);
+  
+            return () => {
+                socket.emit("myInfo", roomId);
+                socket.off("myInfo", getMyInfo);
+            }
+        }
+    }, [socket])
+    
+
+    console.log(myInfo);
 
     return (
         <S.UserInfoWrapper>
             <S.UserWrapper>
-                <S.UserImage />
-                <S.UserName>{userName}</S.UserName>
+                <S.UserImage 
+                    src={image} 
+                    onClick={() => navigate(`/mypage/${id}`)}
+                />
+                <S.UserName onClick={() => navigate(`/mypage/${id}`)}>{nickName}</S.UserName>
             </S.UserWrapper>
-            <S.BanButton $ban={banToggle} onClick={banHandler}>{chatBanText}</S.BanButton>
+            {   // 정지상태 표시
+                myInfo?.isAdmin &&
+                    id !== myInfo.userId &&
+                        <S.BanButton $ban={isJail} onClick={() => setOnAlert(true)}>{isJail ? '채팅정지 취소' : '채팅정지'}</S.BanButton>
+                        
+            }
+            {   // 관리자 표시
+                isAdmin && <S.AdminButton>{admin}</S.AdminButton>
+            }
+            {
+                onAlert ? 
+                    isBrowser ?
+                    <WebAlertModal
+                        setModalOpen={setOnAlert}
+                        title={isJail ? "채팅정지 취소" : "채팅정지"}
+                        content={isJail ? `${nickName}님의 채팅정지를 취소하시겠습니까?` : `${nickName}님의 채팅을 정지하시겠습니까?`}
+                        warning={true}
+                        yesContent={isJail ? "정지 취소" : "정지"}
+                        yesClickHandler={userBanHandler}
+                    />
+                    :
+                    <AppAlertModal 
+                        setModalOpen={setOnAlert}
+                        title={isJail ? "채팅정지 취소" : "채팅정지"}
+                        content={isJail ? `${nickName}님의 채팅정지를 취소하시겠습니까?` : `${nickName}님의 채팅을 정지하시겠습니까?`}
+                        warning={true}
+                        yesContent={isJail ? "정지 취소" : "정지"}
+                        yesClickHandler={userBanHandler}
+                    />
+                    : null
+            }
         </S.UserInfoWrapper>
     )
 }
+
 
 export default ChatUserInfo;
