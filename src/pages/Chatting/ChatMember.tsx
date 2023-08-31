6866a94a2b73fe59532f28a9afbcced71f560b15
[SW5]용지은 <wldms0180@gmail.com>
2023-09-01 03:03:53 +0900

    feat: socket 채팅 멤버 연동 진행 중

--- src/pages/Chatting/ChatMember.tsx
+++ src/pages/Chatting/ChatMember.tsx
@@ -1,10 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import BackButton from '../../components/Chatting/BackButton';
 import ChatUserInfo from '../../components/Chatting/ChatUserInfo';
 import iconSearch from '../../assets/images/svg/ic-search.svg';
 import * as S from './styles/ChatMember.style';
 import ChatSearchMember from '../../components/Chatting/ChatSearchMember';
-import { ChatMemberType } from '../../types/chattingType';
+import { ChatMemberType, FandomType } from '../../types/chattingType';
+import { useLocation } from 'react-router-dom';
+import { SocketContext } from '../../App';
+import { Socket } from 'socket.io-client';
 
 
 const memberMock:ChatMemberType[] = [
@@ -31,7 +34,28 @@ const memberMock:ChatMemberType[] = [
 ];
 
 const ChatMember = () => {
+
+    const socket = useContext<Socket | undefined>(SocketContext);
     const [openSearch, setOpenSearch] = useState<boolean>(false);
+    const { pathname } = useLocation();
+    const roomId = Number(pathname.split("/chatting/chatmember/")[1]);
+    const [members, setMembers] = useState();
+
+    useEffect(() => {
+        if (socket) {
+            const getMember = (v: any) => setMembers(v);
+
+            socket.emit("members", roomId);
+            socket.on("members", getMember);
+
+            return () => {
+                socket.emit("members", roomId);
+                socket.off("members", getMember);
+            }
+        }
+    }, [socket]);
+
+    
 
     return (
         <>
