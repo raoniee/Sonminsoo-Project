77d2c60eabdb9ab190eb8b9e993a0f0bfa47e708
Jieun-Y <106078473+Jieun-Y@users.noreply.github.com>
2023-08-31 21:07:57 +0900

    feat: chat socket 연동

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,13 @@ import { useDispatch } from "react-redux";
 import { setToken } from "./redux/config/rootReducer";
 import useAxiosPrivate from "./hooks/useAxiosPrivate";
 import useSocket from "./hooks/useSocket";
-import { Socket } from "socket.io-client"; 
+import { Socket } from "socket.io-client";
 
 export const SocketContext = createContext<Socket | undefined>(undefined);
 
 const App = () => {
-<<<<<<< HEAD
+  const socket = useSocket();
   const dispatch = useDispatch();
-=======
-    const socket = useSocket();
-    const dispatch = useDispatch();
->>>>>>> c8b0340 (feat: chat socket 연동)
 
   useEffect(() => {
     axios
@@ -32,15 +28,11 @@ const App = () => {
       });
   }, []);
 
-<<<<<<< HEAD
-  return <RouterProvider router={Router} />;
-=======
-    return (
-      <SocketContext.Provider value={socket}>
-        <RouterProvider router={Router} />
-      </SocketContext.Provider>
-    )
->>>>>>> c8b0340 (feat: chat socket 연동)
+  return (
+    <SocketContext.Provider value={socket}>
+      <RouterProvider router={Router} />
+    </SocketContext.Provider>
+  );
 };
 
 export default App;
