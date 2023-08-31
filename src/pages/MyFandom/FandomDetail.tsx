d11d5766e7a99c312b55497402450cdb91263cc9
[SW5] 신혜정 <shj3229123@gmail.com>
2023-08-31 18:54:57 +0900

    feat: 팬덤 디테일 피드 추가

--- src/pages/MyFandom/FandomDetail.tsx
+++ src/pages/MyFandom/FandomDetail.tsx
@@ -5,8 +5,11 @@ import { useParams, useNavigate } from "react-router-dom";
 
 import useAxiosPrivate from "../../hooks/useAxiosPrivate";
 import axios from "../../api/axios";
+import { useParams, useNavigate } from "react-router-dom";
 
-/////////// 팬덤 디테일 import ///////////
+import useAxiosPrivate from "../../hooks/useAxiosPrivate";
+import axios from "../../api/axios";
+
 /////////// 팬덤 디테일 import ///////////
 import HeaderBar from "../../components/common/HeaderBar/HeaderBar";
 import FooterNavBar from "../../components/common/FooterNavBar/FooterNavBar";
@@ -24,8 +27,6 @@ import Comment from "../../components/Feed/Comment";
 import FeedDelete from "../../components/Feed/FeedDelete";
 import AppAlertModal from "../../components/common/AlertModal/AppAlertModal";
 /////////// 팬덤 타입 ///////////
-/////////// 피드 import ///////////
-
 type Fandom = {
   fandomName: string;
   id: number;
