f24f010949031de894012d19750e62fdf0672e1e
[SW5] 신혜정 <shj3229123@gmail.com>
2023-08-31 18:54:57 +0900

    feat: 팬덤 디테일 피드 추가

--- src/pages/MyFandom/FandomDetail.tsx
+++ src/pages/MyFandom/FandomDetail.tsx
@@ -7,8 +7,6 @@ import useAxiosPrivate from "../../hooks/useAxiosPrivate";
 import axios from "../../api/axios";
 
 /////////// 팬덤 디테일 import ///////////
-/////////// 팬덤 디테일 import ///////////
-/////////// 팬덤 디테일 import ///////////
 import HeaderBar from "../../components/common/HeaderBar/HeaderBar";
 import FooterNavBar from "../../components/common/FooterNavBar/FooterNavBar";
 import Notice from "../../components/MyFandom/Notice";
