08a1c7aec99c274a32e92959b2204b2bbb3ec998
Raon <raonharu0822@kakao.com>
2023-08-31 13:37:06 +0900

    Feat: 푸쉬 핀 api 에러 해결

--- src/redux/config/rootReducer.ts
+++ src/redux/config/rootReducer.ts
@@ -3,8 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const rootReducer = createSlice({
   name: "auth",
   initialState: {
-    accessToken:
-      "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjIsImlhdCI6MTY5Mjk1MjI1MywiZXhwIjoxNjk0MTYxODUzfQ.hEXfgQlSsJfiYMDlZXmg_I9bIIf5dLJvPZyOAOfiY14",
+    accessToken: "",
   },
   reducers: {
     setToken: (state, action) => {
