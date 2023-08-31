c4307f6d492449698310120a06b3403bc25bde41
[SW5] 신혜정 <shj3229123@gmail.com>
2023-08-31 18:54:57 +0900

    feat: 팬덤 디테일 피드 추가

--- src/pages/MyFandom/FandomDetail.tsx
+++ src/pages/MyFandom/FandomDetail.tsx
@@ -5,17 +5,25 @@ import { useParams, useNavigate } from "react-router-dom";
 
 import useAxiosPrivate from "../../hooks/useAxiosPrivate";
 import axios from "../../api/axios";
-import { useParams, useNavigate } from "react-router-dom";
 
-import useAxiosPrivate from "../../hooks/useAxiosPrivate";
-import axios from "../../api/axios";
-
+/////////// 팬덤 디테일 import ///////////
 /////////// 팬덤 디테일 import ///////////
 import HeaderBar from "../../components/common/HeaderBar/HeaderBar";
 import FooterNavBar from "../../components/common/FooterNavBar/FooterNavBar";
 import Notice from "../../components/MyFandom/Notice";
 import UpdateFandom from "./UpdateFandom";
 
+/////////// 피드 import ///////////
+import FeedHeader from "../../components/Feed/FeedHeader";
+import FeedText from "../../components/Feed/FeedText";
+import ItemBox from "../../components/Feed/Item";
+import HashTag from "../../components/Feed/HashTag";
+import LikeBtn from "../../components/Feed/LikeBtn";
+import CommentBtn from "../../components/Feed/CommentBtn";
+import Comment from "../../components/Feed/Comment";
+import FeedDelete from "../../components/Feed/FeedDelete";
+import AppAlertModal from "../../components/common/AlertModal/AppAlertModal";
+/////////// 팬덤 타입 ///////////
 /////////// 피드 import ///////////
 import FeedHeader from "../../components/Feed/FeedHeader";
 import FeedText from "../../components/Feed/FeedText";
