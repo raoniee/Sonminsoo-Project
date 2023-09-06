6c18346b40a56f4dd6ece9c7fec16dbb29f7dd32
[SW5] 최재혁 <cslzh4433@gmail.com>
2023-09-06 21:16:02 +0900

    Fix: 마이팬덤 새로고침 시 변경 안되는 버그

--- src/pages/MyFandom/MyFandom.tsx
+++ src/pages/MyFandom/MyFandom.tsx
@@ -5,54 +5,55 @@ import FandomMenu from "../../components/MyFandom/FandomMenu";
 import ContentHeader from "../../components/MyFandom/ContentHeader";
 import MyFandomList from "../../components/MyFandom/MyFandomList";
 import useAxiosPrivate from "../../hooks/useAxiosPrivate";
+import useGetToken from "../../hooks/useGetToken";
 
 import FooterNavBar from "../../components/common/FooterNavBar/FooterNavBar";
 
 type Fandom = {
-    fandomName: string;
-    id: number;
-    lastChatTime: null;
-    memberLength: number;
-    rank: number;
-    image: string;
+  fandomName: string;
+  id: number;
+  lastChatTime: null;
+  memberLength: number;
+  rank: number;
+  image: string;
 };
 
 type FandomData = Fandom[];
 
 const MyFandom = () => {
-    const axiosPrivate = useAxiosPrivate();
-    const [data, setData] = useState<FandomData>([]);
+  const axiosPrivate = useAxiosPrivate();
+  const [data, setData] = useState<FandomData>([]);
+  const auth = useGetToken();
+  useEffect(() => {
+    initDataGet();
+  }, [auth]);
 
-    useEffect(() => {
-        initDataGet();
-    }, []);
+  const initDataGet = async () => {
+    try {
+      const res = await axiosPrivate.get("/fandoms");
+      setData(res.data.data);
+    } catch (error) {
+      console.error("Error", error);
+    }
+  };
 
-    const initDataGet = async () => {
-        try {
-            const res = await axiosPrivate.get("/fandoms");
-            setData(res.data.data);
-        } catch (error) {
-            console.error("Error", error);
-        }
-    };
-
-    return (
-        <>
-            <S.Container>
-                <S.MyFandomHeaderBox>
-                    {/* <S.FandomImg /> */}
-                    <HomeHeader />
-                    <FandomMenu />
-                </S.MyFandomHeaderBox>{" "}
-                <ContentHeader />
-                <S.MyFandomListBox>
-                    {data.map((item) => (
-                        <MyFandomList key={item.id} item={item} />
-                    ))}
-                </S.MyFandomListBox>
-            </S.Container>
-            <FooterNavBar />
-        </>
-    );
+  return (
+    <>
+      <S.Container>
+        <S.MyFandomHeaderBox>
+          {/* <S.FandomImg /> */}
+          <HomeHeader />
+          <FandomMenu />
+        </S.MyFandomHeaderBox>{" "}
+        <ContentHeader />
+        <S.MyFandomListBox>
+          {data.map((item) => (
+            <MyFandomList key={item.id} item={item} />
+          ))}
+        </S.MyFandomListBox>
+      </S.Container>
+      <FooterNavBar />
+    </>
+  );
 };
 export default MyFandom;
