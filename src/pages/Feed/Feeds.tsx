86ce37deeef0524587791cd2917d2d7f46aa0b5c
[SW5] 이주현 <leek71358@gmail.com>
2023-09-27 16:57:55 +0900

    refact:feed페이지 무한스크롤

--- src/pages/Feed/Feeds.tsx
+++ src/pages/Feed/Feeds.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import useAxiosPrivate from "../../hooks/useAxiosPrivate";
 import useAuth from "../../hooks/useAuth";
 import { FeedsType } from "../../types/feed";
@@ -6,21 +6,75 @@ import * as S from "./style/Feed.style";
 import FeedHeaderBar from "../../components/Feed/FeedHeaderBar";
 import FooterNavBar from "../../components/common/FooterNavBar/FooterNavBar";
 import FeedItem from "../../components/Feed/FeedItem";
+import useIntersectionObserver from "../../hooks/useIntersectionObserver";
 
 const Feeds = () => {
   const auth = useAuth();
   const axiosPrivate = useAxiosPrivate();
   const [feedsData, setFeedsData] = useState<FeedsType>([]);
+  const [isLoaded, setIsLoaded] = useState(false);
+  let timerId: NodeJS.Timeout | undefined;
+  let count = 10;
 
   useEffect(() => {
     if (auth.checkIsSignIn) {
       const fetchData = async () => {
-        const res = await axiosPrivate.get("/feeds");
+        const page = 1;
+        const ITEMS_PER_PAGE = 10;
+        const res = await axiosPrivate.get(
+          `/feeds?page=${page}&perPage=${ITEMS_PER_PAGE}`
+        );
         setFeedsData(res.data.data);
       };
       fetchData();
     }
   }, [auth.checkIsSignIn, axiosPrivate]);
+
+  const onIntersect: IntersectionObserverCallback = useCallback(
+    async ([entry], observer) => {
+      if (!!timerId) {
+        console.log("return");
+        return;
+      }
+      if (isLoaded) {
+        console.log("true");
+      }
+      timerId = setTimeout(() => {
+        timerId = undefined;
+      }, 500);
+
+      console.log("timerId", timerId);
+      if (entry.isIntersecting && !isLoaded) {
+        console.log("start");
+        observer.unobserve(entry.target);
+
+        setIsLoaded(() => true);
+        try {
+          count += 1;
+          console.log("count", count);
+          const { data } = await axiosPrivate.get(
+            `/feeds?page=1&perPage=${count}`
+          );
+          // console.log("setItem!", count);
+          setFeedsData(data.data);
+          setIsLoaded(() => false);
+        } catch (err) {
+          setIsLoaded(() => false);
+        }
+
+        observer.observe(entry.target);
+      }
+    },
+    []
+  );
+
+  //현재 대상 및 option을 props로 전달
+  const { setTarget } = useIntersectionObserver({
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.3,
+    onIntersect,
+  });
 
   return (
     <S.FeedContainer>
@@ -28,6 +82,7 @@ const Feeds = () => {
       {feedsData.map((feedData, index) => (
         <FeedItem feedData={feedData} key={`${feedData.id}-${index}`} />
       ))}
+      <div ref={setTarget} />
       <FooterNavBar />
     </S.FeedContainer>
   );
